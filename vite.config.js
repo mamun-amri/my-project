071517f6c0a0e30a7d5ce62eb523c902aa0f283e
mamun-amri <mamunamri97@gmail.com>
2025-09-26 14:12:38 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "https://mamun-amri.github.io/my-project/",
+  base: "/my-project",
 });
